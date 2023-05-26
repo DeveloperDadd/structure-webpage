952c4cceeedbe7e1e7634a6bd651c59e9255ac7e
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-05-26 14:09:09 +0000

    fixed some js code for the buttons to function properly

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ let darkButton = document.getElementById("darkButton");
 
 
 darkButton.addEventListener('click', () => {
-   let element = document.getElementByClass('light')
-   element.classList.remove('light');
-   element.classList.add('dark');
+   let elements = document.getElementsByClassName('light')
+   for (i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('light');
+    elements[i].classList.add('dark');
+   }
+   
 });
 
 lightButton.addEventListener('click', () => {
     let elements = document.getElementsByClassName('dark')
-    elements.classList.remove('dark');
-    elements.classList.add('light');
+    for (i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('dark');
+        elements[i].classList.add('light');
+       }
 });
